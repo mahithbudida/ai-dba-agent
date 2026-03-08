38fbbbb39264830d251df45c3a4b90fd627d6675
mahibmy <mahi.bmy@gmail.com>
2026-03-08 20:21:24 +0530

    Create practise.py

---
+++ practise.py
@@ -0,0 +1,13 @@
+class Person:
+  lastname = ""
+
+  def __init__(self, name):
+    self.name = name
+
+p1 = Person("Linus")
+p2 = Person("Emil")
+
+Person.lastname = "Refsnes"
+
+print(p1.lastname)
+print(p2.name)
